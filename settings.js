438627031bed5daf63a042299afe343c044d50c4
Alex <godwinuzochi51@gmail.com>
2026-04-16 01:03:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
-  packname: 'ᴹᴿ•ᴀʟᴇx᭄',
-  author: 'snowbird',
-  botName: "ᴹᴿ•ᴀʟᴇx᭄",
-  botOwner: 'Snowbird', // Your name
+  packname: 'ᴹᴿ•ᴀʟᴇx md',
+  author: 'ᴹᴿ•ᴀʟᴇx᭄',
+  botName: "ᴹᴿ•ᴀʟᴇx md",
+  botOwner: 'ᴹᴿ•ᴀʟᴇx᭄', // Your name
   ownerNumber: '2347032527540', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
