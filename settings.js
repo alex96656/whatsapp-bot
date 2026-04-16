6c626b77c525081bab08950a2d5b010cc807718b
Alex <godwinuzochi51@gmail.com>
2026-04-16 01:02:50 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,13 @@
-module.exports = {
-  botName: "ᴹᴿ•ᴀʟᴇx᭄ Bot",
-  ownerNumber: "+2347032527540",
-  prefix: ".",
-  mode: "public",
-  autoReply: true
-}
+const settings = {
+  packname: 'ᴹᴿ•ᴀʟᴇx᭄',
+  author: 'snowbird',
+  botName: "ᴹᴿ•ᴀʟᴇx᭄",
+  botOwner: 'Snowbird', // Your name
+  ownerNumber: '2347032527540', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.2.0",
+};
+
+module.exports = settings;
