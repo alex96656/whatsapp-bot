52eeb6dddb31f0e4bbbd49572a4c37b0eec9fac0
Alex <godwinuzochi51@gmail.com>
2026-04-17 02:00:48 +0100

    Delete telegram.js

--- telegram.js
+++
@@ -1,9 +0,0 @@
-const TelegramBot = require("node-telegram-bot-api");
-
-const bot = new TelegramBot(process.env.TG_TOKEN, { polling: true });
-
-function sendTelegramMessage(text) {
-    bot.sendMessage(process.env.TG_CHAT_ID, text);
-}
-
-module.exports = { sendTelegramMessage };
