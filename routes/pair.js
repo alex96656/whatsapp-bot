737cb93cfd1f5bad52d207987b0e7881d5543482
Alex <godwinuzochi51@gmail.com>
2026-04-24 01:35:13 +0100

    Delete pair.js

--- routes/pair.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { generatePairCode } = require("../controllers/pairController");
-
-// POST /api/pair
-router.post("/pair", generatePairCode);
-
-module.exports = router;
