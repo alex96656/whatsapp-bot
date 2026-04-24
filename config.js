5cfbd37a23e848a2c4268a85da7488d0156ec19c
Alex <godwinuzochi51@gmail.com>
2026-04-24 01:42:47 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+// === BOT INFO ===
+export const BotName = "Bot Base for My followers";
+export const BotVersion = "1.0";
+export const OwnerName = "MINATO DEV";
+
+// === NUMBERS ===
+export const OwnerNumber = "242068906671";
+export const CreatorNumber = "242068906671";
+
+// === PREFIX ===
+export const Prefix = ".";
+
+// === MODE ===
+export const Mode = "public"; // public / private
+
+// === MEDIA ===
+export const MenuImage = "bot.png";
+export const MenuAudio = "your-audio.mp3";
+
+// === OPTIONS ===
+export const AutoReact = true;
+export const AutoRead = false;
+
+// === LIENS ===
+export const SupportGroup = "https://t.me/+atk5NMjGDPgyMzk0";
+
+// === FOOTER / SIGNATURE ===
+export const Footer = "© MINATO DEV TECH"
+};
